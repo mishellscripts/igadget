cfff0c02ef76a6cfc8ec644ae8395448a9242935
Michelle Song <michelle@sooryen.com>
2018-09-12 16:03:48 -0700

    Add cart, category, product pages, update routes, update header & footer

--- scripts/sections/footer.js
+++ scripts/sections/footer.js
@@ -1,10 +1,28 @@
 module.exports = function() {
   console.log("Starting scripts/sections/footer.js");
 
+  let $footer = $body.find("#Footer");
+  let $footerSprites = $(tag("div", { class: "sprites-moovweb" }));
+  let $footerPoweredBy = $(tag("p")).text("powered by:");
   let $mwFooter = $(tag("footer", {
     class: "mw-footer"
-  }, "Powered by Moovweb"));
+  }));
+
+  // Modifications to footer inner div
+  let $footerText = $footer.find("p");
+  $footerText.find("a").remove();
+  let $footerSpan = $footerText.find("span");
+  const lines = $footerText.text().split('|')[0].trim().split(/\s\s+/g);
+  $footerText.text("All prices are in ");
+  let $footerCopyright = $(tag("p")).text(lines[1]);
+  $footerSpan.append($footerCopyright);
+  $footerText.append($footerSpan);
+
+  // Append to footer
   $body.append($mwFooter);
+  $mwFooter.prepend($footer);
+  $mwFooter.append($footerPoweredBy);
+  $mwFooter.append($footerSprites);
 
   // This injects a snippet of code used locally with gulp live-reloading
   // Note: assert statements are ignored in production.
--- scripts/sections/header.js
+++ scripts/sections/header.js
@@ -2,5 +2,32 @@ module.exports = function() {
   console.log("Starting scripts/sections/header.js");
 
   let $mwHeader = $(tag("header", { class: "mw-header" }));
+  let $mwHeaderTop = $(tag("div", { class: "mw-header-top" })); // Tag builds new element
+  let $mwHeaderBottom = $(tag("div", { class: "mw-header-bottom" })); // Wrap new element with $() to access form Cheerio object w/ access to methods
+  let $mwMenuBtn = $(tag("div", {class: "mw-menu-btn sprites-menu"}));
+  let $menuIconList = $body.find("#TopMenu ul"); // Create variables expected to be used more than once
+  let $menuIcons = $menuIconList.find("li");
+  let $searchForm = $body.find("#SearchForm form");
+  let $menu = $body.find("#Menu");
+
+  // Initialize header
   $body.prepend($mwHeader);
+  $mwHeader.append($mwHeaderTop);
+  $mwHeader.append($mwHeaderBottom);
+  $mwHeaderTop.append($body.find("#Logo"));
+  $mwHeaderTop.append($menuIconList);
+  $mwHeaderBottom.append($mwMenuBtn);
+  $mwHeaderBottom.append($searchForm);
+
+  // Remove unwanted elements from the menu icons list
+  $menuIcons.not(":contains(Account), :contains(Cart)").remove();
+
+  // Modify search form - Assign placeholder to search input
+  $searchForm.find("input[type=text]").attr("placeholder", "Search...");
+  $searchForm.find("input[type=image]").wrap("div", {class: "mw-search-btn sprites-search"});
+
+  // Move updated menu into header element
+  $mwHeader.append($menu);
+  $menu.append($body.find("#SideCategoryList"));
+  $menu.find("li").addClass("mw-bar-menu");
 };
