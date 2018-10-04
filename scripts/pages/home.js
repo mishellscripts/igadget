94e1974ede728b6b76a6c88581ddaa4c896a7e7b
Michelle Song <michelle@sooryen.com>
2018-10-04 12:56:02 -0700

    Add carousel

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -15,7 +15,18 @@ module.exports = function() {
   $productContentTitle.find(".FeedLink").remove();
   $productContentTitle.addClass("mw-main-title text-center");
 
-  // $productContent.find(".Block").find(".ProductList").find("li").find(".ProductActionAdd").addClass("mw-btn-go");
   $productContent.find(".Block").find(".ProductActionAdd a").addClass("mw-btn mw-btn-go");
 
+  // Create Uranium carousel
+  let $productContentInner = $productContent.find(".BlockContent");
+  let $productList = $productContentInner.find(".ProductList");
+  $productContentInner.attr("data-ur-set", "carousel").attr("data-ur-infinite", "enabled").attr("data-ur-autoscroll", "enabled").attr("data-ur-fill", "1");
+  let $productCarouselDots = $(tag("span")).attr("data-ur-carousel-component", "dots");
+  let $carouselControlPrev = $(tag("a", { class: "mw-control mw-control-prev" })).attr("data-ur-carousel-component", "button").attr("data-ur-carousel-button-type", "prev");
+  let $carouselControlNext = $(tag("a", { class: "mw-control mw-control-next" })).attr("data-ur-carousel-component", "button").attr("data-ur-carousel-button-type", "next");
+  $productContentInner.prepend($carouselControlNext);
+  $productContentInner.prepend($carouselControlPrev);
+  $productContentInner.prepend($productCarouselDots);
+  $productList.attr("data-ur-carousel-component", "scroll_container");
+  $productList.find("li").attr("data-ur-carousel-component", "item");
 };
